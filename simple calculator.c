eea4eaa8bda4d8d2c0700fab4958190fab068bb4
joywairimu <99965130+joywairimu@users.noreply.github.com>
2022-02-18 14:30:23 +0300

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,68 @@
+/*
+simple calulator
+by joyngure
+feb 2022
+MIT licence
+C89 compiler
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{//variable declaration
+    int numl,num2,sum,diff,product;
+    float quotient;
+     printf("simple calculator\n");
+     
+    printf("enter any two numbers:");
+    
+    scanf("%d%d", %num1,%num2);
+    //computation
+    sum=num1+numm2;
+    product=num1+num2
+    quotient=(float)num1/num2;
+    //output
+   printf("%d+%d=%d"\n",num1,num2,sum);
+    printf("%d-%d=%d"\n",num1,num2,diff);
+    printf("%d*%d=%d"\n",num1,num2,product);
+    printf("%d/%d=%d"\n",num1,num2,quotient);
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%d=%d"\n",num1,num2,product)
+;
+    printf("%d+%d=%d",num1,num2,quotient);
+    return 0;
+
+
+
+
+
+        )
+
